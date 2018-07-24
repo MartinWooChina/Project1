d05b3ade1eeecded43842633d279f244287eea8f
武扬 <martinwoochina@outlook.com>
2018-07-24 22:45:25 +0800

    大

---
+++ Project1/demo1.cpp
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+
+
+#include <iostream>
+
+
+
+
+
+
+int main() {
+
+	/*std::cout << "enter two numbers :" << std::endl;
+
+	int v1 = 0; 
+	int v2 = 0;
+
+	std:: cin >> v1 >> v2;
+
+	std::cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std :: endl;
+
+
+	return 0;
+	*/
+
+	int sum = 0, val = 10; 
+	// 小于等于一时 一直循环 
+
+	while (val >= 0) {
+		std::cout << val;
+		std::cout << std::endl;
+
+
+
+		--val;
+
+		
+
+	}
+
+
+	std::cout << "From 10 to 0 "
+		<< sum << std::endl;
+	
+	
+
+	std::cin >> sum;
+
+
+	return 0;
+
+
+
+
+
+
+
+	
+
+}
