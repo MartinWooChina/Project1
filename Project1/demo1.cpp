f402a8c218a7f86cbb8808b0917a387a38155ea9
武扬 <martinwoochina@outlook.com>
2018-07-25 17:47:07 +0800

    test

--- Project1/demo1.cpp
+++ Project1/demo1.cpp
@@ -11,46 +11,27 @@
 
 int main() {
 
-	/*std::cout << "enter two numbers :" << std::endl;
-
-	int v1 = 0; 
-	int v2 = 0;
-
-	std:: cin >> v1 >> v2;
-
-	std::cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std :: endl;
-
-
-	return 0;
-	*/
-
-	int sum = 0, val = 10; 
-	// 小于等于一时 一直循环 
-
-	while (val >= 0) {
-		std::cout << val;
-		std::cout << std::endl;
-
-
-
-		--val;
-
+	int sum = 0;
+	// add from 1 to 10 
+	for (int val = 1; val <= 10; ++val) {
+		sum += val;
+		
 		
 
 	}
-
-
-	std::cout << "From 10 to 0 "
+	std::cout << "sum of 1 to 10 inclusive is "
 		<< sum << std::endl;
-	
-	
 
 	std::cin >> sum;
 
-
 	return 0;
 
-
+	000
+		000
+		000
+		000
+		00
+		00
 
 
 
