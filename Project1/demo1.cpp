84eb679ad655d2740f4e6a36b642bd0b787d2227
武扬 <martinwoochina@outlook.com>
2018-07-27 17:30:03 +0800

    0

--- Project1/demo1.cpp
+++ Project1/demo1.cpp
@@ -11,22 +11,6 @@
 
 int main() {
 
-	int sum = 0;
-	// add from 1 to 10 
-	for (int val = 1; val <= 10; ++val) {
-		sum += val;
-		
-		
-
-	}
-	std::cout << "sum of 1 to 10 inclusive is "
-		<< sum << std::endl;
-
-	std::cin >> sum;
-
-	return 0;
-
-	
 
 
 
